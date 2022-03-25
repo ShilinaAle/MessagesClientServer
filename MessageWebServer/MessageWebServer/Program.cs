fc0ee6c86707c1e997f9bd82876f93c13de5da9e
shilinaale <shilinaale@yandex.ru>
2022-03-25 17:21:14 +0300

    Full server with sqlite

--- MessageWebServer/MessageWebServer/Program.cs
+++ MessageWebServer/MessageWebServer/Program.cs
@@ -2,45 +2,6 @@ using MessageWebServer;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
-//var builder = WebApplication.CreateBuilder(args);
-//var dict = new Dictionary<Guid, List<MessageDTO>>();
-//var app = builder.Build();
-
-//app.MapGet("/", () => "salam maleykum ");
-
-//app.MapGet("/GetAllMessages/{Id}", (Guid id) =>
-//{
-//    if (dict.ContainsKey(id))
-//    {
-//        return dict[id];
-//    }
-//    return new List<MessageDTO>();
-//});
-
-
-//app.MapPost("/SaveMessage", ([FromBody] MessageDTO newMessage) =>
-//{
-//    if (dict.TryGetValue(newMessage.UserId, out List<MessageDTO> userMessages))
-//    {
-//        userMessages.Add(newMessage);
-//    }
-//    else
-//    {
-//        dict[newMessage.UserId] = new List<MessageDTO>() { newMessage };
-//    }
-
-//    return Results.Ok();
-//});
-
-//app.Run();
-
-//public class MessageDTO
-//{
-//    public string Text { get; set; }
-//    public Guid UserId { get; set; }
-//    public string SentDateTime { get; set; }
-//}
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
